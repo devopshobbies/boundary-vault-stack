d55f4bc7367a35225b801e7513e62f481415ca15
Shayan Ghani <shayanghani1384@gmail.com>
2024-07-04 13:07:46 +0330

    added pull images script

---
+++ scripts/pull-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -o pipefail
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
+images=("hashicorp/vault:1.13.3" "bitnami/postgresql:16" "hashicorp/boundary:0.14" "busybox:latest")
+
+for image_name in "${images[@]}"; do
+  docker pull $image_name
+  if [ $? -ne 0 ]; then
+      echo -e "${RED}ERROR : failed to pull image : $image_name${RESET}"
+      exit 1
+  fi
+      echo -e "${GREEN}downloaded $image_name ${RESET}\n"
+done
+echo -e "All images got pulled successfully"
+exit 0
