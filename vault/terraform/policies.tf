ea51262e26067adaca0c41a54bd2096b846398d8
Shayan Ghani <shayanghani1384@gmail.com>
2024-07-06 15:30:39 +0330

    added policies.tf

---
+++ vault/terraform/policies.tf
@@ -0,0 +1,10 @@
+#---------------------
+# Create policies
+#---------------------
+
+resource "vault_policy" "vault_policies" {
+  for_each = var.policies
+  
+  name   = each.key
+  policy = file(each.value)
+}
