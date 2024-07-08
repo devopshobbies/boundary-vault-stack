d5c8c770bf4ad01845e6eb9189c41ee6f3dc6705
Shayan Ghani <shayanghani1384@gmail.com>
2024-07-08 13:45:20 +0330

    added docker linter

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -7,7 +7,7 @@ function lint_terraform(){
     echo -e "Please install Terraform from https://www.terraform.io/downloads.html" >&2
     return 1
   fi
-
+  
   if ! terraform init &> /dev/null; then
     echo "ERROR: Terraform init failed!" >&2
     echo "Ensure you are in the correct directory and check the Terraform configuration." >&2
@@ -19,4 +19,28 @@ function lint_terraform(){
     echo "Check the output above for details on what went wrong." >&2
     return 3
   fi
+
+  return 0
+}
+
+function lint_docker () {
+
+  if ! command -v docker &> /dev/null; then
+    echo -e "ERROR: Docker is not installed!" >&2
+    echo -e "Please install Docker from https://docs.docker.com/get-docker/" >&2
+    return 4
+  fi
+
+  if ! docker compose version &> /dev/null; then
+    echo "WARNING: Docker Version is not 2+"    
+  fi
+
+  if ! docker-compose --version &> /dev/null; then
+    echo "ERROR: Docker Compose is not installed!" >&2
+    echo "Please install Docker Compose from https://docs.docker.com/compose/install/" >&2
+    return 5
+  fi
+
+  return 0
+
 }
