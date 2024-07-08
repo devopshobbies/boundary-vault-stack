309d7f20b8c86f3317eff85d1418f834d5fb7744
Shayan Ghani <shayanghani1384@gmail.com>
2024-07-08 13:43:54 +0330

    added ansible requirements installation

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 ## create ignored dirs in git for confidential data
 mkdir -p logs/ secrets/
 
+## install required collections
+ansible-galaxy collection install -r requirements.yml
+
 ansible-playbook -i ansible/inventory/inventory.ini ansible/playbook.yml --ask-vault-pass
 ## wait 10 seconds for the vault changes to take effect
 sleep 10
